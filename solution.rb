5f1adda9595a28e60009a3c565fd05d2783a3919
mardiniii <sezama03@hotmail.com>
2016-10-13 20:59:39 -0500

    Add solution to token header challenge

--- solution.rb
+++ solution.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  # request.env['HTTP_USER_AGENT']
-  request.user_agent
+  request.env['HTTP_PERMISO'] == 'soy-un-token-secreto' ? 'Si lo logramos!' : 'Sin Permiso'
 end
