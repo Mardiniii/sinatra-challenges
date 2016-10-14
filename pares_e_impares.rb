7b4d0ae940617e6f4b6f7aeecd2f3f21e36f92fd
mardiniii <sezama03@hotmail.com>
2016-10-13 20:20:01 -0500

    Add solution to suma_numeros challenge

---
+++ pares_e_impares.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+
+get '/' do
+  erb :pares_e_impares
+end
--- solution.rb
+++ solution.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  erb :pares_e_impares
+  if params[:counter]
+    @counter = params[:counter].to_i
+  else
+    session[:counter] = 0
+    @counter = session[:counter]
+  end
+  erb :suma_numeros
 end
